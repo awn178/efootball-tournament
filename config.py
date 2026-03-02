2ef84bd3be6562794833046cd2dc528aa2fe5e2c
awn178 <sonblitz549@gmail.com>
2026-03-02 08:24:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ BOT_TOKEN = "8406169991:AAHcP5z7eHiKiSFGlRH3fOSDQS5gkjK-0EM"
 APP_URL = "https://efootball-tournament.onrender.com"
 
 # Admin Configuration
-OWNER_USERNAME = "@awn175"
+OWNER_USERNAME = "awnowner"
+ADMIN_USERNAME = "awnadmin"
 RESULT_SUBMISSION = "@awn178"
 ADMIN_PHONE = "+251961231633"
 
@@ -13,21 +14,14 @@ ADMIN_PHONE = "+251961231633"
 DATABASE_URL = os.environ.get('DATABASE_URL', '')
 
 # Tournament Settings
-DEFAULT_KNOCKOUT_BRACKETS = [
-    {'amount': 30, 'max_players': 16},
-    {'amount': 50, 'max_players': 32},
-    {'amount': 100, 'max_players': 64}
-]
-
-DEFAULT_LEAGUE_BRACKETS = [
-    {'amount': 30, 'max_players': 15},
-    {'amount': 50, 'max_players': 15},
-    {'amount': 100, 'max_players': 15}
-]
+DEFAULT_PRIZES = {
+    '1st': 0,
+    '2nd': 0
+}
 
 # App Settings
 APP_NAME = "eFootball Tournament"
-APP_VERSION = "2.0.0"
+APP_VERSION = "3.0.0"
 DEBUG_MODE = True
 
 # File Upload Settings
@@ -41,16 +35,16 @@ NOTIFY_USER_ON_APPROVAL = True
 NOTIFY_USER_ON_MATCH = True
 
 # Match Settings
-MATCH_DEADLINE_HOURS = 48  # Default 48 hours to play match
-WALKOVER_AFTER_HOURS = 72   # Walkover after 72 hours
+MATCH_DEADLINE_HOURS = 48
+WALKOVER_AFTER_HOURS = 72
 
 # League Settings
 LEAGUE_POINTS_WIN = 3
 LEAGUE_POINTS_DRAW = 1
 LEAGUE_POINTS_LOSS = 0
 
-# Tiebreaker order: Points > Goal Difference > Goals For > Head-to-Head
-LEAGUE_TIEBREAKERS = ['points', 'goal_difference', 'goals_for', 'head_to_head']
+# Tiebreaker order: Points > Goal Difference > Goals For
+LEAGUE_TIEBREAKERS = ['points', 'goal_difference', 'goals_for']
 
 # Pagination
 ITEMS_PER_PAGE = 20
